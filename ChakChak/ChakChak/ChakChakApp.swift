6d2b6bf888bd0efc15bb14d51abd8b9c226f30eb
WonbeenLee <wblee@autocrypt.io>
2026-01-08 21:17:35 +0900

    add : 프로젝트 생성

---
+++ ChakChak/ChakChak/ChakChakApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChakChakApp.swift
+//  ChakChak
+//
+//  Created by 이원빈 on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct ChakChakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
