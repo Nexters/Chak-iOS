f281436156bc00e667e6777d3fd65fa5a07b195f
wongbingg <wb277@naver.com>
2026-01-18 16:13:39 +0900

    [#1] chore: UI 컴포넌트 네이밍 수정
    - EmptyView 가 기존 컴포넌트와 이름이 곂치므로 PhotoEmptyView로 수정

--- Chac/Chac/Main/Components/PhotoEmptyView.swift
+++ Chac/Chac/Main/Components/PhotoEmptyView.swift
@@ -1,5 +1,5 @@
 //
-//  EmptyView.swift
+//  PhotoEmptyView.swift
 //  Chac
 //
 //  Created by 이원빈 on 1/14/26.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct EmptyView: View {
+struct PhotoEmptyView: View {
     
     private enum Strings {
         static let noPhotosMessage = "정리할 사진이 없습니다.\n사진을 올리면 앨범을 착 정리할게요."
@@ -20,5 +20,5 @@ struct EmptyView: View {
 }
 
 #Preview {
-    EmptyView()
+    PhotoEmptyView()
 }
